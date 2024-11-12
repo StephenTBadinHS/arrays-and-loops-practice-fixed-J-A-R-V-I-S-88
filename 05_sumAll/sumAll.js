f1256dae148572452dd58f52bbca6b2cd6e14866
Loeffler <26LoefflerJ@badinhs.org>
2024-11-11 21:35:37 -0500

    Completed 04_removeFromArray and 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,8 @@
-const sumAll = function() {
-
+const sumAll = function(start, end){
+let triStart = ((start - 1) * (start)) / 2
+let triFinal = (end * (end + 1)) / 2
+return triFinal - triStart
 };
 
 // Do not edit below this line
-console.log(sumAll);
+console.log(sumAll(5, 10));
