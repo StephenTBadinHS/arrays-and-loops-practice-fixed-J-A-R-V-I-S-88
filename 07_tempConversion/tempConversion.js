e52ecdd3d27d79967190f98c29df2f70f9dd4926
Loeffler <26LoefflerJ@badinhs.org>
2024-11-11 22:17:38 -0500

    Completed 06_leapYears and 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,10 +1,21 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(F) {
+    let celcius =  (5 / 9) * (F - 32)
+    return Math.round(10 * celcius) / 10
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(C) {
+    let fahrenheit = ((9 / 5) * C) + 32
+    return Math.round(10 * fahrenheit) / 10
 };
 
 // Do not edit below this line
-
-console.log(convertToCelsius);
-console.log(convertToFahrenheit);
+const degreesFromCelcius = [];
+for(i = 0; i <= 100; i++){
+    degreesFromCelcius[i] = convertToFahrenheit(i);
+}
+const degreesFromFahrenheit = [];
+for (i = 0; i <= 212; i++){
+    degreesFromFahrenheit[i] = convertToCelsius(i);
+}
+console.log(degreesFromCelcius);
+console.log(degreesFromFahrenheit);
